0de7488d1eb08954dbf4b1ef9f09627de28449ab
dcorroyer <dylan.corroyer@wanadoo.fr>
2023-11-01 15:29:36 +0100

    chore: migrate monolithe to REST API part1 (bundles)

--- config/bundles.php
+++ config/bundles.php
@@ -1,36 +1,10 @@
 <?php
 
-declare(strict_types=1);
-
 return [
-    Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => [
-        'all' => true,
-    ],
-    Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => [
-        'all' => true,
-    ],
-    Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => [
-        'all' => true,
-    ],
-    Symfony\Bundle\MakerBundle\MakerBundle::class => [
-        'dev' => true,
-    ],
-    Symfony\Bundle\TwigBundle\TwigBundle::class => [
-        'all' => true,
-    ],
-    Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => [
-        'dev' => true,
-        'test' => true,
-    ],
-    Symfony\Bundle\SecurityBundle\SecurityBundle::class => [
-        'all' => true,
-    ],
-    Zenstruck\Foundry\ZenstruckFoundryBundle::class => [
-        'dev' => true,
-        'test' => true,
-    ],
-    Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => [
-        'dev' => true,
-        'test' => true,
-    ],
+    Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
+    Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
+    Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
+    Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
+    Zenstruck\Foundry\ZenstruckFoundryBundle::class => ['dev' => true, 'test' => true],
+    Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
 ];

db64ebd9bbfb5b05864cd24ba6bec1429be162d3
dcorroyer <dylan.corroyer@wyzgroup.com>
2024-02-12 21:48:06 +0100

    add eslint and prettier + apply on existant code

--- assets/layout/App.tsx
+++ assets/layout/App.tsx
@@ -1,18 +1,18 @@
-import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
-import HomePage from "@/layout/HomePage";
-import LoginForm from "@/layout/LoginForm";
+import HomePage from '@/layout/HomePage'
+import LoginForm from '@/layout/LoginForm'
 
 const App = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/login" element={<LoginForm />} />
+        <Route path='/' element={<HomePage />} />
+        <Route path='/login' element={<LoginForm />} />
       </Routes>
     </Router>
-  );
-};
+  )
+}
 
-export default App;
+export default App

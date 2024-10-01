86ab2ac42888ac41d747d21cf42a2eb78c0062bd
dcorroyer <dylan.corroyer@wyzgroup.com>
2024-10-01 15:59:51 +0200

    fix: index css style is not a module

--- app/assets/index.tsx
+++ app/assets/index.tsx
@@ -8,7 +8,7 @@ import '@mantine/core/styles.css'
 import '@mantine/dates/styles.css'
 import '@mantine/notifications/styles.css'
 
-import './index.module.css'
+import './index.css'
 
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { createRouter, RouterProvider } from '@tanstack/react-router'

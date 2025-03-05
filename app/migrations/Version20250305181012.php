206861abdecebb45a90f558204570d8affc4a2de
Dylan Corroyer <dcorroyer@wamiz.com>
2025-03-05 19:40:11 +0100

    fix: budget unique entity

---
+++ app/migrations/Version20250305181012.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20250305181012 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        $this->addSql('DROP INDEX uniq_73f2f77baa9e377a');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE SCHEMA public');
+        $this->addSql('CREATE UNIQUE INDEX uniq_73f2f77baa9e377a ON "budget" (date)');
+    }
+}

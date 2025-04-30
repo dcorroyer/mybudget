d665a040b529c7a54c939065d04e50dbc3ab96b5
Dylan Corroyer <dcorroyer@wamiz.com>
2025-04-30 09:27:42 +0200

    fix migration

--- app/migrations/Version20250429151336.php
+++ app/migrations/Version20250429151336.php
@@ -19,13 +19,13 @@ final class Version20250429151336 extends AbstractMigration
 
     public function up(Schema $schema): void
     {
-        // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE expense ADD payment_method VARCHAR(255) NOT NULL');
+        $this->addSql('ALTER TABLE expense ADD payment_method VARCHAR(255) DEFAULT NULL');
+        $this->addSql("UPDATE expense SET payment_method = 'OTHER' WHERE payment_method IS NULL");
+        $this->addSql('ALTER TABLE expense ALTER COLUMN payment_method SET NOT NULL');
     }
 
     public function down(Schema $schema): void
     {
-        // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE SCHEMA public');
         $this->addSql('ALTER TABLE "expense" DROP payment_method');
     }
